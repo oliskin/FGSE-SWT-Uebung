01da72cac464fccf4a5d237a0e3396eabe1c977d
oliskin <olga.liskin@gmail.com>
2013-11-03 18:03:22 +0100

    Nächste Änderung

--- HelloRemoteGitProject/src/de/fgse/hrg/MainA.java
+++ HelloRemoteGitProject/src/de/fgse/hrg/MainA.java
@@ -6,7 +6,7 @@ public class MainA {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Text der ersten Person");
+		System.out.println("Neue Änderung");
 
 	}
 

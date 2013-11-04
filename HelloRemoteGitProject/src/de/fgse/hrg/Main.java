9163994fbebc78b3ead2ff647c39f0b33c1ffb98
okarras <o.karras@gmail.com>
2013-11-04 10:08:08 +0100

    Neue Git-Erkenntinisse

--- HelloRemoteGitProject/src/de/fgse/hrg/Main.java
+++ HelloRemoteGitProject/src/de/fgse/hrg/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String... args){
 		System.out.println("Hello Git");
 		System.out.println("More Git!");
+		System.out.println("EVEN More Git!");
 	}
 
 }
